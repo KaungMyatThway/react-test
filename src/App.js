bdc03809862402927de25a8ae78e6a478f2c5f76
Kaung Myat Thway <kaungmyatthway1997@gmail.com>
2025-05-08 11:24:52 +0630

    conditions rendering test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ import { useState } from "react";
 
 function App() {
   let [name, setName] = useState("Kaung Myat Thway");
+  let [posts,setPosts] = useState([
+    {
+      id : 1,
+      title: "first post"
+    },
+    {
+      id : 2,
+      title: "second post"
+    },
+    {
+      id : 3,
+      title: "third post"
+    }
+  ])
+
   let changeName = () => {
     name = "aung aung";
     setName(name);
@@ -15,6 +30,15 @@ function App() {
       <h1>{name}</h1>
       <img src={Image} alt="Sample"></img>
       <button onClick={changeName}>Change Name</button>
+
+      <h1>Post Lists</h1>
+      <ul>
+        {
+          posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
