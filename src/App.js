8f7df6731dea8d0e8f0aa2856ca4cf32a3e4d01a
Kaung Myat Thway <kaungmyatthway1997@gmail.com>
2025-05-08 11:01:58 +0630

    useState test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Image from './ss.jpeg'
+import logo from "./logo.svg";
+import "./App.css";
+import Image from "./ss.jpeg";
+import { useState } from "react";
 
 function App() {
-  let name = "Kaung Myat Thway"
+  let [name, setName] = useState("Kaung Myat Thway");
+  let changeName = () => {
+    name = "aung aung";
+    setName(name);
+  };
+
   return (
     <div className="App">
-       <h1>{name}</h1>
-       <img src={Image} alt="Sample"></img>
+      <h1>{name}</h1>
+      <img src={Image} alt="Sample"></img>
+      <button onClick={changeName}>Change Name</button>
     </div>
   );
 }
